9413caca11450440bd3a18e92039163ede070c6b
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-04-27 10:29:20 +0300

    adding application to admin dashboard, blank=true in models

--- iti/students/models.py
+++ iti/students/models.py
@@ -5,6 +5,12 @@ from django.db import models
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
-    age = models.IntegerField(null=True)
-    email = models.EmailField(max_length=200, unique=True, null=True)
-    grade = models.IntegerField(null=True)
+    age = models.IntegerField(null=True, blank=True)
+    # null=True --> db --/// blank=True--> admin
+    email = models.EmailField(max_length=200, unique=True, null=True, blank=True)
+    grade = models.IntegerField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return self.name
